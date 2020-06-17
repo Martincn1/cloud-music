c215ef01c492281e00493f45887910e3e68b5aa0
shao_bin <shaobin@sohh.cn>
2020-06-17 15:53:31 +0800

    add style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Hello from "./Hello";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello />
+      <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
